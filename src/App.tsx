093a634788b1d6f6d5939c487946ed72c0793596
chvmvd <104971044+chvmvd@users.noreply.github.com>
2023-03-19 15:04:24 +0900

    Create color picker

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,72 @@
-import React from "react";
+import React, { useState } from "react";
 import AzarashiSvg from "./components/AzarashiSvg";
+import { Paper, Stack, Typography } from "@mui/material";
 
 function App(): JSX.Element {
+  const [bodyColor, setBodyColor] = useState("#ffffff");
+  const [eyeColor, setEyeColor] = useState("#000000");
+  const [cheekColor, setCheekColor] = useState("#FBBABA");
+  const [mouseColor, setMouseColor] = useState("#DCDCDC");
   return (
     <>
+      <Paper sx={{ p: 2 }}>
+        <Stack spacing={1}>
+          <Stack>
+            <Typography variant="h6" component="h2">
+              Body Color
+            </Typography>
+            <input
+              type="color"
+              value={bodyColor}
+              onChange={(e) => {
+                setBodyColor(e.target.value);
+              }}
+            />
+          </Stack>
+          <Stack>
+            <Typography variant="h6" component="h2">
+              Eye Color
+            </Typography>
+            <input
+              type="color"
+              value={eyeColor}
+              onChange={(e) => {
+                setEyeColor(e.target.value);
+              }}
+            />
+          </Stack>
+          <Stack>
+            <Typography variant="h6" component="h2">
+              Cheek Color
+            </Typography>
+            <input
+              type="color"
+              value={cheekColor}
+              onChange={(e) => {
+                setCheekColor(e.target.value);
+              }}
+            />
+          </Stack>
+          <Stack>
+            <Typography variant="h6" component="h2">
+              Mouse Color
+            </Typography>
+
+            <input
+              type="color"
+              value={mouseColor}
+              onChange={(e) => {
+                setMouseColor(e.target.value);
+              }}
+            />
+          </Stack>
+        </Stack>
+      </Paper>
       <AzarashiSvg
-        bodyColor="white"
-        mouseColor="#DCDCDC"
-        cheekColor="#FBBABA"
-        eyeColor="black"
+        bodyColor={bodyColor}
+        eyeColor={eyeColor}
+        cheekColor={cheekColor}
+        mouseColor={mouseColor}
         beardNumber={2}
       />
     </>
